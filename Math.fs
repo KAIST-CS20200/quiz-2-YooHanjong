f18d33781024a64d6704fe01990196d2f6ae8241
YooHanjong <103297718+YooHanjong@users.noreply.github.com>
2025-03-11 10:10:26 +0900

    Update Math.fs

--- Math.fs
+++ Math.fs
@@ -5,5 +5,9 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let collatz n =
-  0
+let collatz  (n ) =
+  let rec iter product count =
+    if product=1UL then count
+    else if product%2UL=0UL then iter (product/2UL) (count+1)
+      else iter (3UL*product+1UL) (count+1)
+  iter (uint64 n) 0
